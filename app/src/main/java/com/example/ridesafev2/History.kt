ac65d64b20133280097e51123b2b4a55bb7427ef
SirChubbx17 <qjccdelacruz@tip.edu.ph>
2022-12-15 22:34:02 +0800

    fixed history

--- app/src/main/java/com/example/ridesafev2/History.kt
+++ app/src/main/java/com/example/ridesafev2/History.kt
@@ -1,4 +1,4 @@
-package com.example.ridesafe
+package com.example.ridesafev2
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,15 +6,36 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.ridesafev2.R
+import com.example.ridesafev2.databinding.RsFragHistoryBinding
 
 class History : Fragment() {
+
+    private var _binding: RsFragHistoryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.rs_frag_history, container, false)
+
+        _binding = RsFragHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        binding.buttonSecond.setOnClickListener {
+//            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+//        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
